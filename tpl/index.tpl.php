51e850b213c6145ab265920f2cca00be8e8fdafc
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2025-09-26 09:08:24 +0200

    Extract main teaser into separate tpl

--- tpl/index.tpl.php
+++ tpl/index.tpl.php
@@ -1,62 +1,24 @@
 <?php
-/**
- * @var array $categorizedLessons
- */
 
 $html['title']       = 'Learn Git';
 $html['description'] = 'Learn Git';
 $html['url']         = 'https://lerngit.online/';
 $html['image']       = 'https://example.com/image.jpg';
+
 ?>
 <?php include self::TPL_DIR . 'layout/head.tpl.php'; ?>
 
     <?php include self::TPL_DIR . 'layout/header.tpl.php'; ?>
 
     <main>
-        <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center">
-            <div class="col-md-6 p-lg-5 mx-auto my-5">
-                <h1 class="display-3 fw-bold"><?= $tagline ?></h1>
-                <h3 class="fw-normal text-muted mb-3">Learn Git the easy way</h3>
-                <div class="d-flex gap-3 justify-content-center lead fw-normal">
-                    <a class="icon-link" href="#how-it-works">
-                        Learn more
-                        <svg class="bi" aria-hidden="true"><use xlink:href="#chevron-right"></use></svg>
-                    </a>
-                    <a class="icon-link" href="/lessons">
-                        Start right now
-                        <svg class="bi" aria-hidden="true"><use xlink:href="#chevron-right"></use></svg>
-                    </a>
-                </div>
-                <div class="mt-5">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 585 265">
-                        <g transform="translate(67 252)">
-                            <path class="git-history-path" d="M-25-125v11h17c8 0 15 7 15 15v63A26 26 0 0 0 33-9h80v-12H33a15 15 0 0 1-15-15v-63c0-6-2-11-5-15h34v-11H-8Z" overflow="visible"/>
-                            <path class="git-history-path" stroke-width="11.3" d="M87-120h119M154-15h80M419-224h49M359-120h81"/>
-                            <path class="git-history-path" d="M301-230c-14 0-26 12-26 26v63c0 9-7 16-15 16h-17v11h72v-11h-34c3-5 5-10 5-16v-63c0-8 7-15 15-15h80v-11z" overflow="visible"/>
-                            <circle class="git-history-main" cx="-40.1" cy="-119.7" r="20.8" stroke-width="11.3"/>
-                            <circle class="git-history-main" cx="64.5" cy="-119.7" r="20.8" stroke-width="11.3"/>
-                            <circle class="git-history-main" cx="223.1" cy="-119.7" r="20.8" stroke-width="11.3"/>
-                            <circle class="git-history-main" cx="335" cy="-119.7" r="20.8" stroke-width="11.3"/>
-                            <circle class="git-history-main" cx="457.4" cy="-120.6" r="20.8" stroke-width="11.3"/>
-                            <circle class="git-history-feature1" cx="133.2" cy="-15" r="20.8" stroke-width="11.3"/>
-                            <circle class="git-history-feature1" cx="253.8" cy="-15" r="20.8" stroke-width="11.3"/>
-                            <circle class="git-history-feature2" cx="397.3" cy="-225.3" r="20.8" stroke-width="11.3"/>
-                            <circle class="git-history-feature2" cx="489.9" cy="-225.3" r="20.8" stroke-width="11.3"/>                                                        
-                        </g>
-                    </svg>
-                </div>
-            </div>
-
-            <div class="product-device shadow-sm d-none d-md-block"></div>
-            <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
-        </div>
+        <?php include self::TPL_DIR . 'home/teaser.tpl.php'; ?>
 
         <?php include self::TPL_DIR . 'home/symbols.tpl.php'; ?>
 
         <?php include self::TPL_DIR . 'home/how-it-works.tpl.php'; ?>
 
         <?php include self::TPL_DIR . 'home/contribute.tpl.php'; ?>
-        
+
         <?php include self::TPL_DIR . 'home/thanks.tpl.php'; ?>
 
     </main>
